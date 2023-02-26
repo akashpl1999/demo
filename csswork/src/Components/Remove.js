86f43814c83b3f72f924542c3dd9f1e83d279fe4
akashpl1999 <akash111pl@gmail.com>
2023-02-26 11:41:33 +0530

    upadted

---
+++ csswork/src/Components/Remove.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Remove = () => {
+  return (
+  
+  
+          <div>
+
+
+
+
+        REmove
+        
+        
+        
+        </div>
+
+        
+  )
+}
+
+export default Remove
